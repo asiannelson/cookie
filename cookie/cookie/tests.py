179e81bdeac7a811f53d7e8def9cd7f9fdf71f17
asiannelson <nelson14h@yahoo.com>
2017-02-24 11:13:46 -0700

    pyrmaid

---
+++ cookie/cookie/tests.py
@@ -0,0 +1,28 @@
+import unittest
+
+from pyramid import testing
+
+class ViewTests(unittest.TestCase):
+	def setUp(self):
+		self.config = testing.setUp()
+
+	def tearDown(self):
+		testing.tearDown()
+
+	def test_my_view(self):
+		from .views import my_view
+		request = testing.DummyRequest()
+		info = my_view(request)
+		self.assertEqual(info['project'], 'cookie')
+		self.assertEqual(info['nickname'], 'fuzzy forest')
+
+class FunctionalTests(unittest.TestCase):
+	def setUp(self):
+		from pyramidcookie import main
+		app = main({})
+		from webtest import TestApp
+		self.testapp = TestApp(app)
+
+	def test_root(self):
+		res = self.testapp.get('/', status=200)
+		self.assertTrue(b'Pyramid' in res.body)
